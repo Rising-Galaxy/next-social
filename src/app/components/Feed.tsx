18ec622090f0679c7252e49f8af04d0c3a53315e
Galactic Starlit <2982388220@qq.com>
2025-02-08 22:23:24 +0800

    进一步完善 Post Card 设计

--- src/app/components/Feed.tsx
+++ src/app/components/Feed.tsx
@@ -2,7 +2,7 @@ import Post from './Post'
 
 const Feed = () => {
   return (
-    <div className="p-4 bg-white shadow-md rounded-lg flex-col gap-12">
+    <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
       <Post />
       <Post />
       <Post />
